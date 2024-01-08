31949b0c8d4305247f547d926d11b083cfab026d
Darren Laser <darren.laser@gmail.com>
2024-01-08 11:42:26 -0500

    updated environment filezx

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  weatherKey: 'b3e9d7f392e76dcad37d28662a81d603', // would normally be stored in a secret manager
+  weatherKey: import.meta.env.NG_APP_WEATHER_URL,
   api: {
     host: 'api.openweathermap.org',
   },
